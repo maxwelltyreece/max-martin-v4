cf47c98e3d91a1de683915bb0f152dc393e8a1e7
Maxwell <107719479+maxwelltyreece@users.noreply.github.com>
2026-01-27 23:06:06 +0000

    Update page.tsx

--- src/app/me/page.tsx
+++ src/app/me/page.tsx
@@ -3,7 +3,6 @@
 import Image from "next/image";
 import { motion } from "framer-motion";
 
-
 export default function Me() {
     return (
         <motion.div>
@@ -20,17 +19,27 @@ export default function Me() {
                         <source src="/animations/me_anim.mp4" type="video/mp4" />
                     </video>
                 </div>
+
                 <div className="bio-content">
                     <Image
                         src="/gallery/Profile Picture.png"
                         alt="Profile Picture"
                         width={200}
-                        height={200}/>
+                        height={200}
+                    />
 
-                    <p> Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec in sagittis massa, ultrices facilisis diam. Praesent maximus ex libero, non gravida lorem sodales eget. Quisque tortor felis, dapibus in feugiat non, dignissim in augue. Nulla non diam vel urna tristique laoreet. Etiam cursus dignissim eros, non scelerisque tellus sagittis condimentum. Integer purus enim, tempus sed augue in, pulvinar tristique ligula. Mauris imperdiet nulla sit amet pharetra porta. Nunc vel mi sagittis augue placerat tincidunt consequat sit amet quam. In hac habitasse platea dictumst. Vivamus auctor commodo risus, ac mattis mi. Vestibulum elementum dui justo, non finibus enim lobortis eget. Vestibulum.</p>
+                    <p>
+                        I’m a computer science student and multidisciplinary creative operating at the
+                        intersection of music, technology, and design. I’m looking for high-impact
+                        roles in music tech and application-focused industries where creative
+                        experimentation and technical depth are valued.
+                    </p>
 
-                    <p> Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec in sagittis massa, ultrices facilisis diam. Praesent maximus ex libero, non gravida lorem sodales eget. Quisque tortor felis, dapibus in feugiat non, dignissim in augue. Nulla non diam vel urna tristique laoreet. Etiam cursus dignissim eros, non scelerisque tellus sagittis condimentum. Integer purus enim, tempus sed augue in, pulvinar tristique ligula. Mauris imperdiet nulla sit amet pharetra porta. Nunc vel mi sagittis augue placerat tincidunt consequat sit amet quam. In hac habitasse platea dictumst. Vivamus auctor commodo risus, ac mattis mi. Vestibulum elementum dui justo, non finibus enim lobortis eget. Vestibulum. </p>
-
+                    <p>
+                        I thrive in environments where I can contribute to a community, build
+                        distinctive digital experiences, and bring together engineering, music, and
+                        creative direction to solve problems in original ways.
+                    </p>
                 </div>
             </div>
         </motion.div>
